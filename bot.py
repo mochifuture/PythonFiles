92daedfef87af187e38558b94a0be408b3690548
mochi <mochi@Mac.local>
2020-06-10 00:23:36 +0900

    Degrade チュートリアル_01

--- bot.py
+++ bot.py
@@ -1,13 +1,3 @@
-# 常にTrue（正）でプログラムを繰り返し処理する
 while True:
-  # 入力をおうむ返しする
-  command = input('はなしかける>')
-
-  # in演算子
-  if 'こんにちは' in command:
-      print('いいお天気ですね')
-  elif 'おしまい' in command:
-      print('さようなら')
-      break
-  else:
-     print(command)
+  input('はなしかける>')
+  print('こんにちは')
